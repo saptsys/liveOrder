8b872acf7446026d252dcb8bb718d27af754f58e
Keval <kevald47@gmail.com>
2019-01-26 16:11:19 +0530

    updated

--- screens/Admin/index.php
+++ screens/Admin/index.php
@@ -3,5 +3,7 @@
 
     if( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ){
             echo $_SESSION["user"]." is an ".$_SESSION["role"];
+    }else{
+        header('Location: ../../index.php');
     }
 ?>
--- screens/Waiter/index.php
+++ screens/Waiter/index.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
     if( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ){
-            echo $_SESSION["user"]." is an ".$_SESSION["role"];
+        require_once "../../auth.php";
+        echo $_SESSION["user"]." is an ".$_SESSION["role"];
+    }else{
+        header('Location: ../../index.php');
     }
 ?>
