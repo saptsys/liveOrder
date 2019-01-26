135b57592ecc8ebed83ae687cdd15a0886989241
Keval <kevald47@gmail.com>
2019-01-26 14:22:14 +0530

    Create loginAuth.php

---
+++ loginAuth.php
@@ -0,0 +1,16 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] != "POST") exit();
+    
+    $allok=false;
+    if ( (isset($_POST['username'])) && (isset($_POST["password"])) ) {
+       if( ($_POST['username'] != "") && ($_POST['password'] != "") ){
+            $allok=true;
+       }else $allok=false;
+    }else $allok = false;
+    if($allok) {
+        require_once "auth.php";
+        $auth=new authenticate($_POST["username"],$_POST["password"],true);
+    }else{
+        header('Location: login.php');
+    }
+?>
