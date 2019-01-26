c186b3a423383f4734be679e1e5625c5377a24d0
Keval <kevald47@gmail.com>
2019-01-26 12:02:46 +0530

    create diffrent screens for diffrent roles

---
+++ screens/Admin/index.php
@@ -0,0 +1,7 @@
+<?php
+    session_start();
+
+    if( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ){
+            echo $_SESSION["user"]." is an ".$_SESSION["role"];
+    }
+?>
---
+++ screens/Waiter/index.php
@@ -0,0 +1,6 @@
+<?php
+    session_start();
+    if( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ){
+            echo $_SESSION["user"]." is an ".$_SESSION["role"];
+    }
+?>
