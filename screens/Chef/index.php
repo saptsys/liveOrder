4c1f3d37b6e862bfe8b5bc55038193dd15ed89dc
Keval <kevald47@gmail.com>
2019-01-26 16:12:52 +0530

    updated

--- screens/Chef/index.php
+++ screens/Chef/index.php
@@ -2,6 +2,8 @@
     session_start();
 
     if( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ){
-            echo $_SESSION["user"]." is an ".$_SESSION["role"];
+        echo $_SESSION["user"]." is an ".$_SESSION["role"];
+    }else{
+        header('Location: ../../index.php');
     }
 ?>
--- screens/Waiter/index.php
+++ screens/Waiter/index.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     if( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ){
-            echo $_SESSION["user"]." is an ".$_SESSION["role"];
+        echo $_SESSION["user"]." is an ".$_SESSION["role"];
+    }else{
+        header('Location: ../../index.php');
     }
 ?>
