c8150f0d6660a81783aba007564e3701be29c82b
Keval <kevald47@gmail.com>
2019-01-26 14:22:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-  header('Location: login.php');
+    $ok=false;
+    if( (isset($_COOKIE['user'])) && ( isset($_COOKIE['pass']) ) ){
+        $user=$_COOKIE['user'];
+        $pass=$_COOKIE['pass'];
+        require_once "auth.php";
+        $auth=new authenticate($user,$pass,false);
+        if($auth->authenticated) $ok=true;
+    }
+    if(!$ok) header('Location: login.php');
 ?>
