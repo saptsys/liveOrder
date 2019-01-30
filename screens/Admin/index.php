b2b68ee34eb73e1e7e79cbdfaa26aef221f7292a
Keval <kevald47@gmail.com>
2019-01-30 16:34:37 +0530

    Update index.php

--- screens/Admin/index.php
+++ screens/Admin/index.php
@@ -1,8 +1,26 @@
 <?php
     session_start();
-    if( ! ( (isset($_SESSION["user"])) && (isset($_SESSION["pass"])) && (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"])) ) ){
-        header('Location: ../../index.php');
+    if( ( (isset($_COOKIE['user'])) && (isset($_COOKIE['pass'])) ) ){
+        if( !( isset($_SESSION['user']) && isset($_SESSION['pass']) && isset($_SESSION['role']) ) ){
+            require_once '../../auth.php';
+            $auth = new authenticate($_COOKIE['user'],$_COOKIE['pass'],false,true);
+            if(!$auth->authenticated) header('Location: ../../');
+            else{
+                header('Location: ../../');
+                exit();
+            }
+        }else{
+            if(! ($_SESSION['role'] == "Admin") ){
+                header('Location: ../../');
+            }
+        }
+    }else{
+        header('Location: ../../');
+        exit();
     }
+    include "../../config.php";
+    $mycon = new config();
+    $con = $mycon->db();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +35,6 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
 
     <link rel="stylesheet" href="../../assets/css/Index-style.css">
     <script src="../../assets/js/script.js"></script>
