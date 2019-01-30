b0a558704c4c8f904b8b1a606116c0ba012d9a87
Keval <kevald47@gmail.com>
2019-01-30 15:13:23 +0530

    Update header.php

--- component/header.php
+++ component/header.php
@@ -1,3 +1,6 @@
 <div id="header">
-    <h1><i class="fas fa-glass-cheers"></i><b> Live Orders</b><button class='about-btn btn'>Logout @<?php echo $_SESSION['user'] ?></button></h1>
-</div>
+        <h1><i class="fas fa-glass-cheers"></i><b> Live Orders</b><button onclick="logout()" class='logout-btn btn'>Logout
+        <br> <?php
+            echo '@'.$_SESSION['user'];
+        ?></button></h1>
+</div><!--#header-->
