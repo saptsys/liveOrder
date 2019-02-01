367b920960a7d64a469ac9901fc328d09ca9504a
Keval <kevald47@gmail.com>
2019-02-01 18:50:59 +0530

    Update action.php

--- screens/Chef/action.php
+++ screens/Chef/action.php
@@ -8,6 +8,12 @@
     else echo "<script>window.location='index.php';</script>";
     
     if($flag=="getOrders") getOrders($con);
+
+    //under construction
+    if($flag=="checkDb"){
+        $id=$_POST['id'];
+        checkDb($con,$id);
+    } 
     if($flag=="orderReady"){
         $id=$_POST['id'];
         orderReady($con,$id);
@@ -18,29 +24,34 @@
     }
 
     function getOrders($con)  {  
-        $data = mysqli_query($con,"SELECT * FROM `livetableorders` WHERE NOT(Pending = 0) ORDER BY id");
-        while($row = mysqli_fetch_array($data)) { 
-            $dish="";
-            $productSQL=mysqli_query($con,"SELECT * FROM `products` WHERE Id = '$row[ProductId]' ");
-            $pData=mysqli_fetch_array($productSQL);
-            $catSQL=mysqli_query($con,"SELECT * FROM `categories` WHERE Id='$pData[CatId]'");
-            $cData=mysqli_fetch_array($catSQL);
-            $dish=$cData['Name']."/".$pData['Name'];
-            echo'
-                <tr id="row'.$row['Id'].'">
-                    <td>'.$dish.'</td>
-                    <td>'.$row["Pending"].'</td>
-                    <td> 
-                        <button type="button" class="btn btn-primary" onclick="orderReady('.$row['Id'].')">
-                            <i class="fa fa-check"></i>
-                        </button>
-                        <button type="button" class="btn btn-primary" onclick="orderDeclined('.$row['Id'].')">
-                            <i class="fa fa-times"></i>
-                        </button>
-                    </td>
-                </tr>
-            ';
+        $data = mysqli_query($con,"SELECT * FROM `livetableorders` WHERE NOT(Pending = 0) ORDER BY time");
+        if(mysqli_affected_rows($con) != 0){
+            while($row = mysqli_fetch_array($data)) { 
+                $dish="";
+                $productSQL=mysqli_query($con,"SELECT * FROM `products` WHERE Id = '$row[ProductId]' ");
+                $pData=mysqli_fetch_array($productSQL);
+                $catSQL=mysqli_query($con,"SELECT * FROM `categories` WHERE Id='$pData[CatId]'");
+                $cData=mysqli_fetch_array($catSQL);
+                $dish=$cData['Name']."/".$pData['Name'];
+                echo'
+                    <tr id="row'.$row['Id'].'">
+                        <td>'.$dish.'</td>
+                        <td>'.$row["Pending"].'</td>
+                        <td> 
+                            <button type="button" class="btn btn-primary" onclick="orderReady('.$row['Id'].')">
+                                <i class="fa fa-check"></i>
+                            </button>
+                            <button type="button" class="btn btn-danger" onclick="orderDeclined('.$row['Id'].')">
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </td>
+                    </tr>
+                ';
+            }
+        }else{
+            echo "no orders...";
         }
+       
     }
     function orderReady($con,$id){
         $data = mysqli_query($con,"UPDATE `livetableorders` SET Quantity=Quantity+Pending,Pending=0 WHERE id=$id");
@@ -49,6 +60,36 @@
     function orderDeclined($con,$id)
     {
         $data = mysqli_query($con,"UPDATE `livetableorders` SET Pending=0 WHERE id=$id");
-        echo "deleted for".$id;
+    }
+    //this function is under constructio..
+    function checkDb($con,$id){
+        $stmt="SELECT id FROM `livetableorder` WHERE id > $id";
+        if(mysqli_query($con,$stmt)){
+            if(msql_affected_rows($con) != 0){
+                $data = mysqli_query($con,"SELECT * FROM `livetableorders` WHERE NOT(Pending = 0) AND time > $time ORDER BY time");
+                while($row = mysqli_fetch_array($data)) { 
+                    $dish="";
+                    $productSQL=mysqli_query($con,"SELECT * FROM `products` WHERE Id = '$row[ProductId]' ");
+                    $pData=mysqli_fetch_array($productSQL);
+                    $catSQL=mysqli_query($con,"SELECT * FROM `categories` WHERE Id='$pData[CatId]'");
+                    $cData=mysqli_fetch_array($catSQL);
+                    $dish=$cData['Name']."/".$pData['Name'];
+                    echo'
+                        <tr id="row'.$row['Id'].'">
+                            <td>'.$dish.'</td>
+                            <td>'.$row["Pending"].'</td>
+                            <td> 
+                                <button type="button" class="btn btn-primary" onclick="orderReady('.$row['Id'].')">
+                                    <i class="fa fa-check"></i>
+                                </button>
+                                <button type="button" class="btn btn-danger" onclick="orderDeclined('.$row['Id'].')">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    ';
+                }
+            }else echo "null";
+        }else echo "null";
     }
 ?>
