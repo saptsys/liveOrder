841b255e29029b3c869725338be19598bce25da5
Keval <kevald47@gmail.com>
2019-02-02 16:25:04 +0530

    Update action.php

--- screens/Chef/action.php
+++ screens/Chef/action.php
@@ -24,7 +24,7 @@
     }
 
     function getOrders($con)  {  
-        $data = mysqli_query($con,"SELECT * FROM `livetableorders` WHERE NOT(Pending = 0) ORDER BY time");
+        $data = mysqli_query($con,"SELECT * FROM `kitchen` WHERE NOT(Pending = 0) ORDER BY time");
         if(mysqli_affected_rows($con) != 0){
             while($row = mysqli_fetch_array($data)) { 
                 $dish="";
@@ -32,12 +32,12 @@
                 $pData=mysqli_fetch_array($productSQL);
                 $catSQL=mysqli_query($con,"SELECT * FROM `categories` WHERE Id='$pData[CatId]'");
                 $cData=mysqli_fetch_array($catSQL);
-                $dish=$cData['Name']." / ".$pData['Name'];
+                $dish=$cData['Name']." / ".$pData['Name']." @ ".$row['TableId'];
                 echo'
                     <tr id="row'.$row['Id'].'">
                         <td>'.$dish.'</td>
                         <td>'.$row["Pending"].'</td>
-                        <td> 
+                        <td>
                             <button type="button" class="btn btn-primary" onclick="orderReady('.$row['Id'].')">
                                 <i class="fa fa-check"></i>
                             </button>
@@ -51,22 +51,21 @@
         }else{
             echo "no orders...";
         }
-       
     }
     function orderReady($con,$id){
-        $data = mysqli_query($con,"UPDATE `livetableorders` SET Quantity=Quantity+Pending,Pending=0 WHERE id=$id");
+        $data = mysqli_query($con,"UPDATE `kitchen` SET Quantity=Quantity+Pending,Pending=0 WHERE id=$id");
     
     }
     function orderDeclined($con,$id)
     {
-        $data = mysqli_query($con,"UPDATE `livetableorders` SET Pending=0 WHERE id=$id");
+        $data = mysqli_query($con,"UPDATE `kitchen` SET Pending=0 WHERE id=$id");
     }
     //this function is under constructio..
     function checkDb($con,$id){
         $stmt="SELECT id FROM `livetableorder` WHERE id > $id";
         if(mysqli_query($con,$stmt)){
             if(msql_affected_rows($con) != 0){
-                $data = mysqli_query($con,"SELECT * FROM `livetableorders` WHERE NOT(Pending = 0) AND time > $time ORDER BY time");
+                $data = mysqli_query($con,"SELECT * FROM `kitchen` WHERE NOT(Pending = 0) AND time > $time ORDER BY time");
                 while($row = mysqli_fetch_array($data)) { 
                     $dish="";
                     $productSQL=mysqli_query($con,"SELECT * FROM `products` WHERE Id = '$row[ProductId]' ");
