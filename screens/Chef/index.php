b8b3f27e56a72eee3cb9a389177b44b418e8d34d
vaitul <vaitulb@gmail.com>
2019-02-04 23:38:53 +0530

    setted tab favicon

--- screens/Chef/index.php
+++ screens/Chef/index.php
@@ -29,6 +29,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home : Chef</title>
+    <link rel="icon" href="https://image.flaticon.com/icons/png/512/65/65155.png">
     <link rel="stylesheet" href="../../assets/css/header.css">
     <script src="../../assets/js/script.js"></script>
     <link rel="stylesheet" href="./assets/css/style.css">
--- screens/Waiter/index.php
+++ screens/Waiter/index.php
@@ -29,6 +29,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home : Waiter</title>
+    <link rel="icon" href="https://image.flaticon.com/icons/png/512/65/65155.png">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK">
