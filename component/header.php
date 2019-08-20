fd274b818e68fa57662f8b32811b73fcd031f92e
vaitul <vaitul@VAITUL-LAPTOP>
2019-08-20 23:19:37 +0530

    added background css effect

--- component/header.php
+++ component/header.php
@@ -1,6 +1,6 @@
 <div id="header">
         <h1><i class="fas fa-glass-cheers"></i><b> Live Orders</b><button onclick="logout()" class='logout-btn btn'>Logout
         <br> <?php
-            echo '@'.$_SESSION['user'];
+            echo ucwords($_SESSION['fullName']);
         ?></button></h1>
 </div><!--#header-->
