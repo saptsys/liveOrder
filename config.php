0bafab269875b39a776327bb41b60680312cfe63
vaitul <vaitul@VAITUL-LAPTOP>
2019-09-10 12:22:15 +0530

    ++

--- config.php
+++ config.php
@@ -6,14 +6,14 @@
 			public $db="liveorder";
 			
 			public function db(){
-				if(@$conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db) or die("Can not connect to the database.<br><a href='Database/initialize.php'>Database Management</a>")){
+				if(@$conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db) or die("Can not connect to the database. check <br><b>Database Management</b>")){
 					return $conn;
 				}else{
 					die("Connect failed: %s\n". $conn -> error);
 				}
 			}
 			public function dbOnlyCon(){
-				if(@$conn = mysqli_connect($this->host, $this->user, $this->pass) or die("Can not connect to the database.<br><a href='Database/initialize.php'>Database Management</a>")){
+				if(@$conn = mysqli_connect($this->host, $this->user, $this->pass) or die("Can not connect to the database. check <br><b>Database Management</b>")){
 					return $conn;
 				}else{
 					die("Connect failed: %s\n". $conn -> error);
